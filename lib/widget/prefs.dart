0d91492af8b13b8f7d1f2a90eeaacc7cf6d1bfe5
Alexander Linder <alexander-linder@gmx.de>
2020-09-27 04:53:28 +0200

    Return Future on string save

--- lib/widget/prefs.dart
+++ lib/widget/prefs.dart
@@ -1,8 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-void saveString(String key, String value) async {
+Future<bool> saveString(String key, String value) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setString(key, value);
+  return prefs.setString(key, value);
 }
 
 Future<String> getString(String key) async {
